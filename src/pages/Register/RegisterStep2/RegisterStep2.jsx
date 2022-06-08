709795478e58cca20432b07193ad166188045bb6
hiamthach <dreon1008@gmail.com>
2022-06-08 09:11:55 +0700

    update 7/6

--- src/pages/Register/RegisterStep2/RegisterStep2.jsx
+++ src/pages/Register/RegisterStep2/RegisterStep2.jsx
@@ -1,15 +1,39 @@
 import React, {useEffect} from 'react'
+import './register-step2.scss'
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import { Link, useParams } from "react-router-dom"
+
 import ArrowButton from "../../../components/ArrowButton/ArrowButton"
+import Button from '../../../components/Button/index'
+import CustomInput from "../../../components/CustomInput"
+
+const schema = yup.object({
+  username: yup.string().required("Bạn phải nhập tài khoản").matches(/^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/,  "Vui lòng nhập lại tài khoản"),
+  email: yup.string().required("Bạn phải nhập email").matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, "Vui lòng nhập lại email"),
+  password: yup.string().required("Bạn phải nhập password").matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Vui lòng nhập lại mật khẩu"),
+  passwordConfirmation: yup.string().oneOf([yup.ref('password'), null], 'Mật khẩu chưa đúng')
+}).required()
 
 export default function RegisterStep2(props) {
   let { roleId } = useParams()
+
+  const { register, handleSubmit, formState: {errors}} = useForm({resolver: yupResolver(schema)})
+
   return (
     <div>
     RegisterStep2
-    <Link to='/register'>
-      <ArrowButton text="Trở lại" direction="left"/>
-    </Link>
+
+    <div className="register-step2__btns">
+      <Link to='/register' className="register-step2__btns--back">
+        <ArrowButton text="Trở lại" direction="left"/>
+      </Link>
+      <div className="register-step2__btns--submit">
+        <Button name="ĐĂNG KÝ"/>
+      </div>
+    </div>
     </div>
   )
 }
