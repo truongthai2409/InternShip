f503311269c90a55ce0cb5febeacf05104b5a149
Nguyễn Văn Lộc <vanloc087@gmail.com>
2022-06-09 17:20:20 +0700

    update: add company admin 9/6

--- src/containers/Admin/CompanyForm/index.jsx
+++ src/containers/Admin/CompanyForm/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Avatar, Grid, Switch } from "@mui/material";
@@ -45,8 +45,18 @@ export default function CompanyForm(props) {
 
   // handle Submit form
   const onSubmit = (data) => {
-    console.log(data);
-    dispatch(addCompany(data));
+    
+    const companyData = {
+      description: data.description,
+      email: data.email,
+      logo: null,
+      name: data.name,
+      phone: data.phone,
+      tax: data.tax,
+      website: data.website
+  }
+
+    dispatch(addCompany(companyData));
   };
 
   return (
@@ -98,7 +108,7 @@ export default function CompanyForm(props) {
                     type="text"
                     placeholder="Tên công ty..."
                     register={register}
-                    value="abc sdasd"
+                    // value="abc sdasd"
                     check={!isEdit}
                   >
                     {errors.name?.message}
--- src/store/reducers/company.reducer.js
+++ src/store/reducers/company.reducer.js
@@ -1,9 +1,12 @@
 import {
   ADD_COMPANY_SUCCESS,
   ADD_COMPANY_FAIL,
+  GET_COMPANY_LIST_SUCCESS,
+  GET_COMPANY_LIST_FAIL
 } from "../constants/company.constant";
 
 const initialState = {
+  companyList: [],
   notification: {},
 };
 
@@ -13,6 +16,10 @@ const companyReducer = (state = initialState, { type, payload }) => {
       return { ...state, notification: payload };
     case ADD_COMPANY_FAIL:
       return { ...state };
+    case GET_COMPANY_LIST_SUCCESS:
+      return {...state, companyList: payload}
+    case GET_COMPANY_LIST_FAIL:
+      return {...state, notification: payload}
     case "CLOSE_NOTIFICATION": {
       state.notification.open = payload;
       return { ...state };
