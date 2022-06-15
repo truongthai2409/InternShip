46b24ee742c5db7f5a3f62268a903608f64fecac
hiamthach <dreon1008@gmail.com>
2022-06-15 09:38:34 +0700

    update register slice

---
+++ src/store/slices/notifications/notificationSlice.js
@@ -0,0 +1,12 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../../config/api/apiConfig";
+
+const notificationSlice = createSlice({
+    name: "notification",
+    initialState: {
+
+    },
+    reducers: {}
+})
+
+export default notificationSlice
