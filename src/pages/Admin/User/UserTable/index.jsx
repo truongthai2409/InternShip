03038173802502f53789227d0706186f66fa5a2c
hoangvu6262 <hoangvubg6262115@gmail.com>
2022-06-15 09:30:40 +0700

    update: 15/6

--- src/pages/Admin/User/UserTable/index.jsx
+++ src/pages/Admin/User/UserTable/index.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import DataTable from "../../../../components/Table";
-import { getUserList } from "../../../../store/actions/user.action";
+// import { getUserList } from "../../../../store/actions/user.action";
 
 const UserTable = () => {
   const dispatch = useDispatch();
@@ -14,9 +14,9 @@ const UserTable = () => {
 
   const { userList } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    dispatch(getUserList());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(getUserList());
+  // }, []);
 
   const columns = [
     { field: "stt", headerName: "STT", width: 100 },
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import registerSlice from "./slices/register/registerSlice";
 
 const store = configureStore({
