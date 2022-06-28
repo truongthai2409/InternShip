e51c3492d175baea99b3b3bde42c28157c74d81a
duytaovo <duytaovo@gmail.com>
2022-06-28 22:19:08 +0700

    update-home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // import PropTypes from 'prop-types'
-import Button from '../Button'
-import Logo from '../Logo';
+import Button from "../Button";
+import Logo from "../Logo";
 import "./styles.scss";
 
 function Header(props) {
   return (
     <div className="container">
-      <Logo/>
+      <Logo />
       <div className="login__home">
         <Link to="/login" className="login">
           <h5>Đăng Nhập</h5>
         </Link>
-        <Button name="Đăng Ký" />
+        <Link to="/register">
+          <Button name="Đăng Ký" />
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-Header.propTypes = {}
+Header.propTypes = {};
 
-export default Header
+export default Header;
--- src/components/SearchResultHome/index.jsx
+++ src/components/SearchResultHome/index.jsx
@@ -1,43 +1,52 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 import { IconButton } from "@mui/material";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import AddLocationIcon from '@mui/icons-material/AddLocation';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AddLocationIcon from "@mui/icons-material/AddLocation";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
-import Button from "../../components/Button/"
+import Button from "../../components/Button/";
+import CustomSelect from "../CustomSelect";
 import "./styles.scss";
+import SelectAreaHome from "../SelectAreaHome";
+
+const listTypeJobs = ["TPHCM", "HN", "Đà Nẵng"];
+
 function SearchResultHome(props) {
   return (
     <div className="header__with-search">
-     <div className="header__with-search-search">
-      <div className="header__with-search-search-wrap">
+      <div className="header__with-search-search">
+        <div className="header__with-search-search-wrap">
           <IconButton className="search__icon">
             <SearchOutlinedIcon />
           </IconButton>
-        <input type="text" className="header__with-search-search-1" required id="none" placeholder="Tìm Kiếm" />
-      </div>
-      <div className="header__with-search-search-select">
-            <IconButton className="select__icon-location">
-              <AddLocationIcon />
-            </IconButton>
-            <span className="header__with-search-search-select-option header__with-search-search-select-option-boder ">
-              Khu vực
-            </span>
-            <IconButton className="select__icon-arrow-drop">
-              <ArrowDropDownIcon />
-            </IconButton>
-      </div>
-      <div className="header__with-search-button-search">
-         <Button name="Tìm kiếm" ></Button>
+          <input
+            type="text"
+            className="header__with-search-search-1"
+            required
+            id="none"
+            placeholder="Tìm Kiếm"
+          />
+        </div>
+        <div className="header__with-search-search-select">
+          <IconButton className="select__icon-location">
+            <AddLocationIcon />
+          </IconButton>
+          {/* <CustomSelect></CustomSelect> */}
+          <SelectAreaHome label="Khu vực:" selectOptions={listTypeJobs} />
+          {/* <IconButton className="select__icon-arrow-drop">
+            <ArrowDropDownIcon />
+          </IconButton> */}
+        </div>
+        <div className="header__with-search-button-search">
+          <Button name="Tìm kiếm"></Button>
+        </div>
       </div>
     </div>
-</div>
-
-  )
+  );
 }
 
-SearchResultHome.propTypes = {}
+SearchResultHome.propTypes = {};
 
-export default SearchResultHome
+export default SearchResultHome;
