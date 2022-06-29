a0c8da9b671409fb8a96002fb62c9fe61884f12a
duytaovo <duytaovo@gmail.com>
2022-06-29 16:49:02 +0700

    home-detail

--- src/components/SideBarHomeList/index.jsx
+++ src/components/SideBarHomeList/index.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.scss";
 import Tabs from "@mui/material/Tabs";
 import CustomCheckbox from "../CustomCheckbox";
+import { useDispatch, useSelector } from "react-redux";
+import { getMajorList } from "../../store/slices/Admin/major/majorSlice";
 
 const listTypeJobs = ["Full time", "Part time", "Remote"];
-const listPositions = ["Front end", "Back end", "Fullstack", "Mobile", "Embedded", "Tester", "DevOps"];
-const listMajors = [
-    "Khoa học máy tính",
-    "Công nghệ phần mềm",
-    "Kỹ thuật máy tính",
-    "Trí tuệ nhân tạo",
-    "Kỹ thuật mạng",
-    "Hệ thống quản lý thông tin",
+const listPositions = [
+  "Front end",
+  "Back end",
+  "Fullstack",
+  "Mobile",
+  "Embedded",
+  "Tester",
+  "DevOps",
 ];
 
 function SideBarHomeList(props) {
-    return (
-        <div className="slideBarHome__wrapper">
-            {/* <Tabs
+  const { majorList } = useSelector((state) => state.major);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMajorList());
+  }, []);
+  return (
+    <div className="slideBarHome__wrapper">
+      {/* <Tabs
                 style={{height:'100%'}}
                 orientation="vertical"
                 variant="scrollable"
@@ -26,26 +33,26 @@ function SideBarHomeList(props) {
             >
                 
             </Tabs> */}
-            <div className="slideBarHome__container">
-                <h3 className="slideBarHome__title">Loại việc</h3>
-                {listTypeJobs.map((typeJobs) => (
-                    <CustomCheckbox key={typeJobs} label={typeJobs} />
-                ))}
-            </div>
-            <div className="slideBarHome__container">
-                <h3 className="slideBarHome__title">Vị trí</h3>
-                {listPositions.map((position) => (
-                    <CustomCheckbox key={position} label={position} />
-                ))}
-            </div>
-            <div className="slideBarHome__container">
-                <h3 className="slideBarHome__title">Chuyên ngành</h3>
-                {listMajors.map((major) => (
-                    <CustomCheckbox key={major} label={major} />
-                ))}
-            </div>
-        </div>
-    );
+      <div className="slideBarHome__container">
+        <h3 className="slideBarHome__title">Loại việc</h3>
+        {listTypeJobs.map((typeJobs) => (
+          <CustomCheckbox key={typeJobs} label={typeJobs} />
+        ))}
+      </div>
+      <div className="slideBarHome__container">
+        <h3 className="slideBarHome__title">Vị trí</h3>
+        {listPositions.map((position) => (
+          <CustomCheckbox key={position} label={position} />
+        ))}
+      </div>
+      <div className="slideBarHome__container">
+        <h3 className="slideBarHome__title">Chuyên ngành</h3>
+        {majorList.map((major, i) => (
+          <CustomCheckbox key={major.id} label={major.name} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SideBarHomeList;
