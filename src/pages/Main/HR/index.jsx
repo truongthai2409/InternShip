14f50e7d628bf4ef7a8dfb20211c8c33a92bef22
duytaovo <duytaovo@gmail.com>
2022-07-06 15:33:18 +0700

    ff'

--- src/pages/Main/HR/index.jsx
+++ src/pages/Main/HR/index.jsx
@@ -4,10 +4,7 @@ import Home from "../Home";
 const HR = (props) => {
   return (
     <div className="main__layout">
-<<<<<<< HEAD
       {/* <HeaderWithHR /> */}
-=======
->>>>>>> 9a7ed797be62f59c3a8ee5f46515fb6a2f4928d3
       <Home></Home>
     </div>
   );
