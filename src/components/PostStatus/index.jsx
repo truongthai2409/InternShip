21a45abd50f27f0e0f4217340c3c9166051cb51d
box <duytuong100101@gmail.com>
2022-07-06 15:41:48 +0700

    update HRPostList page 3h41' - 06/07

---
+++ src/components/PostStatus/index.jsx
@@ -0,0 +1,33 @@
+import "./styles.scss";
+
+
+
+const PostStatus = (props) => {
+
+    let text = 'Trạng thái không có', statusClass = 'not-available'
+
+    switch (props.status){
+        case 1: {
+            text="Đang đăng tuyển"
+            statusClass= "active"
+            break;
+        }
+        case 4: {
+            text="Đã đóng"
+            statusClass= "disable"
+            break;
+        }
+        default:{
+
+        }
+    }
+
+    return (
+        <div className={`post-status__container ${statusClass}`}>
+            <p className="post-status__text">{text}</p>
+        </div>
+    )
+}
+
+
+export default PostStatus;
