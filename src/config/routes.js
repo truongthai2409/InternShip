367adbf52b58d63bc876b961aa1b9f12c8709094
box <duytuong100101@gmail.com>
2022-07-07 13:22:15 +0700

    update 1h22'-07/07

--- src/config/routes.js
+++ src/config/routes.js
@@ -20,7 +20,7 @@ import MajorDetail from "../pages/Admin/MajorDetail";
 import Location from "../pages/Admin/Location";
 import DetailHome from "../pages/Main/DetailHome";
 import HR from "../pages/Main/HR";
-import HRPost from "../pages/Main/HRPost";
+import HRPost from "../pages/Main/HR/HRPost";
 import HRPostList from "../pages/Main/HR/HRPostList";
 
 export const adminRouter = [
