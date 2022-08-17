046ea6e82f678bf14ffd4cb95d1523f819ebc5f0
Le Quy <lequytlh2119@gmail.com>
2022-08-17 14:59:28 +0700

    update responsive modal when register HR account success at 17/08 15h00

--- src/Layouts/Login/index.jsx
+++ src/Layouts/Login/index.jsx
@@ -12,7 +12,7 @@ import { ModalContent } from "./components";
 const LoginLayout = () => {
   const { status } = useSelector((state) => state.register);
 
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -32,12 +32,20 @@ const LoginLayout = () => {
 
       <div className="login-container-wrapper">
         <LoginContainer />
-        <Modal
-          modalTitle="Thông báo"
-          open={open}
-          setOpen={setOpen}
-          children={<ModalContent onClick={handleClose} nameButton="Đồng ý" />}
-        />
+        <div className="login-container__modal">
+          <Modal
+            modalTitle="Thông báo"
+            open={open}
+            setOpen={setOpen}
+            children={
+              <ModalContent
+                onClick={handleClose}
+                nameButton="Đồng ý"
+                className="login-container__modal--content"
+              />
+            }
+          />
+        </div>
       </div>
 
       <Footer />
