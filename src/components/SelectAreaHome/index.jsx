b1f97b54619ac263a241d158a062cc89c8d8eb7d
Nothing <auquangtuan@gmail.com>
2022-08-25 10:27:41 +0700

    update -- 10h27' - 25/08

--- src/components/SelectAreaHome/index.jsx
+++ src/components/SelectAreaHome/index.jsx
@@ -63,7 +63,7 @@ export default function SelectAreaHome({ onChange }) {
             }}
             MenuProps={MenuProps}
             sx={{
-              mr: 0,
+              mr: 2,
             }}
             multiple={false}
             displayEmpty
@@ -74,7 +74,7 @@ export default function SelectAreaHome({ onChange }) {
             {provinceList.map((province) => (
               <MenuItem
                 key={province.id}
-                value={province.name}
+                value={((province.name).length > 10) ? (province.name.slice(0,9).concat("...")) : (province.name)}
                 // style={getStyles(province.id, personName, theme)}
               >
                 {province.name}
