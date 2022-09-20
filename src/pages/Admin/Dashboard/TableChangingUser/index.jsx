d1b8efa65a18c762671e4a5b9ca0c3b3cc2e284d
Nguyễn Trung Hiếu <73569473+nthieu1332002@users.noreply.github.com>
2022-09-20 14:13:19 +0700

    layout admin dashboard - 2h13 -20/9/2022

---
+++ src/pages/Admin/Dashboard/TableChangingUser/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TableChangingUser = () => {
+  return (
+    <div className="table-container">
+      <h3>LƯỢT THAY ĐỔI GẦN NHẤT</h3>
+      <table></table>
+    </div>
+  );
+};
+
+export default TableChangingUser;
