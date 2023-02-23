1f14fb8cbb9c7ca2a30849b47d4a1b5d280388ae
duydinh1469 <duydinh1469@gmail.com>
2023-02-23 11:07:03 +0700

    fix hr

--- src/pages/Main/Home/CommingSoon/index.jsx
+++ src/pages/Main/Home/CommingSoon/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import Footer from 'src/components/Footer';
+// import Footer from 'src/components/Footer';
+import Footer from 'src/components/shared/Footer/Footer';
 import HeaderContainer from 'src/components/HeaderContainer';
 
 const CommingSoon = () => {
-    return <div style={{textAlign: 'center'}}>
-        <HeaderContainer/>
-        <img width={"80%"} height={"80%"} src="https://repository-images.githubusercontent.com/279611541/b7e1e580-c611-11ea-9b24-523c65baea0e" alt="CommingSooon" />
-        <Footer/>
-    </div>;
+    return (
+        <>
+            <div style={{textAlign: 'center'}}>
+                <HeaderContainer/>
+                <img width={"80%"} height={"80%"} src="https://repository-images.githubusercontent.com/279611541/b7e1e580-c611-11ea-9b24-523c65baea0e" alt="CommingSooon" />
+                
+            </div>
+            <Footer/>
+        </>
+    )
     
 }
 
